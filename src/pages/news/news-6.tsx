7270a3e5591553ba890366464e65c3769f9d6fa1
Adham Tarek <adhamtarek291@gmail.com>
2022-12-09 09:44:34 +0200

    feat: ff

--- src/pages/news/news-6.tsx
+++ src/pages/news/news-6.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import Footer from '@/components/footer/Footer';
-import Header from '@/components/header/Header';
 import HeroSectionSM from '@/components/hero-section-sm/HeroSectionSM';
 import Layout from '@/components/layout/Layout';
 import NewsDetailSection6 from '@/components/news-detail-section/NewsDetailSection6';
@@ -16,13 +14,11 @@ export default function NewDetailPage6() {
       <main>
         <div className='news-detail-page'>
           <PageShape />
-          <Header />
           <HeroSectionSM
             image='/images/header-bg/news-bg-6.png'
             name='AAAM, AfCFTA brainstorm on how to fast-track devt of African automobile sector'
           />
           <NewsDetailSection6 />
-          <Footer />
         </div>
       </main>
     </Layout>
